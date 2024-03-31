db953b5bb2eaf77881e9a07f2a621e43817dfcf6
Kieran Emery <kizwiz@hotmail.co.uk>
2024-03-31 04:31:30 +0100

    Add project files.

---
+++ Profile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOYA
+{
+    public class Profile
+    {
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        public Profile(string name, int age, string city = "N/A", string country = "N/A", string pronouns = "they/them")
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+            this.country = country;
+            this.pronouns = pronouns;
+            this.hobbies = new string[0];
+        }
+
+        public string ViewProfile()
+        {
+            string bio = $"Name: {name},\n Age: {age},\n City: {city},\n Country: {country},\n Pronouns: {pronouns}";
+            bio += ",\n Hobbies:\n";
+            foreach (string hobby in hobbies)
+            {
+                bio += $"- {hobby}\n";
+            }
+            return bio;
+        }
+
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OOYA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile sam = new Profile("Sam Drakkila", 30);
+            sam.SetHobbies(new string[] {
+        "Running",
+        "reading",
+        "gaming",
+        "painting"
+        });
+            Console.WriteLine(sam.ViewProfile());
+        }
+    }
+}
